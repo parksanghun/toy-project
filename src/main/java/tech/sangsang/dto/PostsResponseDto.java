31fe2cd28dcd9965875a9996a99837e3536fa796
sanghun <sanghun1984@gmail.com>
2020-07-22 16:18:50 +0900

    init project

---
+++ src/main/java/tech/sangsang/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package tech.sangsang.dto;
+
+import lombok.Getter;
+import tech.sangsang.domain.posts.Posts;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
